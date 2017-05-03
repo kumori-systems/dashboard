00b3da6261ddbbc161a18ee808ab97c318dadc6b
Óscar Muñoz Garriós <omunoz@iti.es>
2017-05-03 11:50:44 +0200

    avanzando vista overview

--- src/components/innerComponents/DeploymentCard.vue
+++ src/components/innerComponents/DeploymentCard.vue
@@ -1,45 +1,57 @@
 <template>
     <div class="card">
-        <div class="card-header title"
-             v-bind:class="state">
+        <div class="card-header title" v-bind:class="state">
             <span class="card-header-left">{{title}}</span>
             <span class="card-header-right">{{identificador}}</span>
         </div>
         <div class="card-body">
             <p><u>Service:</u> {{service}}</p>
-            <div class="roles"
-                 v-if="roles && roles.lenght">
-                <p>Roles:</p>
-                {{roles}}
+            <div class="roles" v-if="roles">
+                <p><u>Roles:</u></p>
+                <div v-for="rol, key in roles" class="rol">
+                    <span>
+                    <strong>{{key}}</strong><span class="right">{{getNumInstances(rol)}}</span>
+                    </span>
+                    <p>{{rol.entrypoint.domain}}</p>
+                </div>
             </div>
             <p><u>Website:</u> {{website}}</p>
             <p><u>Links:</u> {{links}}</p>
             <p><u>Volumes:</u> {{volumes}}</p>
         </div>
-        <div class="card-footer"
-             v-if="false"></div>
+        <div class="card-footer" v-if="false" />
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import { Rol } from '../../connection';
 
 @Component({
-    name:'deployment-card',
+    name: 'deployment-card',
     props: {
-        state: String,
-        title: String,
-        identificador: String,
-        service: String,
-        roles: Object,
-        website: String,
-        links: String,
-        volumes: String
+        state: { required: true, type: String },
+        title: { required: true, type: String },
+        identificador: { required: true, type: String },
+        service: { required: true, type: String },
+        roles: { required: true, type: Rol },
+        website: { required: true, type: String },
+        links: { required: true, type: String },
+        volumes: { required: true, type: String }
     }
 })
 export default class Card extends Vue {
-    mounted(){}
+    roles: Rol = this.roles;
+
+    /**
+     * Método para obtener el número de instáncias de un rol
+     * */
+    getNumInstances(rol): number{
+        let res = 0;
+        for (let key in rol.instances)res++;
+        return res;
+    }
 }
 </script>
 
@@ -54,10 +66,13 @@ $padding: 10px;
 .title {
     padding: $padding;
 }
+.rol{
+    padding-left: $padding;
+}
 
 .card-header-right {
     color: grey;
-    padding-left:10px;
+    padding-left: padding;
 }
 
 .normal {
--- src/components/views/Overview.vue
+++ src/components/views/Overview.vue
@@ -1,52 +1,67 @@
 <template>
     <div>
-        <input type="checkbox" id="hideEntryPoints" v-model="hideEntrypoints"/>
+    
+        <input type="checkbox" id="hideEntryPoints" v-model="hideEntrypoints" />
         <label for="hideEntryPoints" v-on:click="hideEntrypoints(null)"> Hide HTTP entrypoints</label>
-
-        <div class="tile" v-for="deployment in deploymentList">
-            <deployment-card 
+    
+        <div class="tile" v-for="deployment, key in deploymentList">
+            <deployment-card
                 v-bind:key="deployment.service"
+                
                 v-bind:state="'normal'"
-                v-bind:title="'HTTP Entrypoint'"
+                v-bind:title="getDeploymentName(key)"
+                v-bind:identificador="getDeploymentId(key)"
                 v-bind:class="'entrypoint'"
-                v-bind:identificador="'mi identificador'"
                 v-bind:service="deployment.service"
                 v-bind:roles="deployment.roles"
                 v-bind:website="'mi website'"
                 v-bind:links="'mis links'"
-                v-bind:volumes="'mis volumenes'" />
+                v-bind:volumes="'mis volumenes'"/>
         </div>
+    
     </div>
 </template>
-
 <script lang="ts">
-
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
 // Componentes
-import DeploymentCard from './../innerComponents/DeploymentCard.vue'
-import {Collapse, Item as CollapseItem}from 'vue-bulma-collapse'
+import DeploymentCard from './../innerComponents/DeploymentCard.vue';
+import { Collapse, Item as CollapseItem } from 'vue-bulma-collapse';
+import { Deployment } from '../../connection';
 
-@Component({  
+@Component({
     name: 'Overview',
-    components:{
+    components: {
         'deployment-card': DeploymentCard,
-        'collapse':Collapse,
-        'collapse-item':CollapseItem
+        'collapse': Collapse,
+        'collapse-item': CollapseItem
     }
 })
-export default class Overview extends Vue{
+export default class Overview extends Vue {
+
+    // Methods
+    getDeploymentName(deploymentNameId: string): string {
+        return deploymentNameId.substring(0, deploymentNameId.lastIndexOf('-'));
+    }
+    getDeploymentId(deploymentNameId: string): string {
+        return deploymentNameId.substring(deploymentNameId.lastIndexOf('-')+1, deploymentNameId.length);
+    }
+
+
     // computed
-    get deploymentList(){ // Obteniendo lista de deployments
+    /**
+     * Obteniendo lista de deployments
+     */
+    get deploymentList(): { [key: string]: Deployment } {
         return this.$store.getters.getDeploymentList;
     }
 
-    get hideEntrypoints(){
+    get hideEntrypoints(): boolean {
         return this.$store.getters.getHideEntrypoints;
     }
 
-    set hideEntrypoints({}){
+    set hideEntrypoints({ }) {
         this.$store.dispatch('hideEntrypoints', {});
     }
 }
