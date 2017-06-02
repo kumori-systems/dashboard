02b00cca1ab4f103e6e14c7a10930457f7d0ab2a
Óscar Muñoz Garriós <omunoz@iti.es>
2017-06-02 14:07:06 +0200

    empezando implementacion detallada de vista elements

--- src/components/views/Elements.vue
+++ src/components/views/Elements.vue
@@ -1,16 +1,49 @@
 <template>
     <div>
-        <input type="checkbox" id="showpublic3rdpartyelements">
-        <label for="showpublic3rdpartyelements">Show public 3rd party elements</label>
+        <div class="tile">
+            <span class="control has-icon">
+                <input class="input" placeholder="Search">
+                <span class="icon is-small">
+                    <i class="fa fa-search"></i>
+                </span>
+            </span>
+            <button class="button">Download manifest</button>
+            <button class="button">Delete</button>
+        </div>
+        <p>
+            <input type="checkbox" id="showpublic3rdpartyelements">
+            <label for="showpublic3rdpartyelements">Show public 3rd party elements</label>
+        </p>
         <collapse>
             <collapse-item title="Components">
-                <p v-for="component in componentList" v-bind:key="component">{{component}}</p>
+                <p v-for="component in componentList" v-bind:key="component">{{component}}
+                    <button class="button">
+                        <i class="fa fa-info" aria-hidden="true" />
+                    </button>
+                    <button class="button">delete</button>
+                    <button class="button">create new deployment</button>
+                    <input type="checkbox" id="selected">
+                </p>
             </collapse-item>
             <collapse-item title="Services">
-                <p v-for="service in serviceList" v-bind:key="service">{{service}}</p>
+                <p v-for="service in serviceList" v-bind:key="service">{{service}}
+                    <button class="button">
+                        <i class="fa fa-info" aria-hidden="true" />
+                    </button>
+                    <button class="button">delete</button>
+                    <button class="button">create new deployment</button>
+                    <input type="checkbox" id="selected">
+                </p>
             </collapse-item>
             <collapse-item title="Runtimes">
-                <p v-for="runtime in runtimeList" v-bind:key="runtime">{{runtime}}</p>
+                <p v-for="runtime in runtimeList" v-bind:key="runtime">{{runtime}}
+                    <button class="button">
+                        <i class="fa fa-info" aria-hidden="true" />
+                    </button>
+                    <button class="button">delete</button>
+                    <button class="button">create new deployment</button>
+                    <input type="checkbox" id="selected">
+                </p>
             </collapse-item>
         </collapse>
     </div>
@@ -31,7 +64,7 @@ import { FabElement } from '../../store/classes';
     }
 })
 export default class Elements extends Vue {
-     mounted() {
+    mounted() {
         let fabElementsList: Array<FabElement> = [];
         this.$store.dispatch('setFabElements', { fabElementsList: fabElementsList });
     }
