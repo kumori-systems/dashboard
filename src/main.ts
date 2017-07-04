071a80c8a9059336e11d384cd206ed6475dcf4ed
Óscar Muñoz Garriós <omunoz@iti.es>
2017-07-04 12:17:23 +0200

    cambiando a la version sin compilador (recomendado)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App.vue';
 
@@ -10,22 +8,10 @@ import store from './store';
 import { sync } from 'vuex-router-sync';
 sync(store, router);
 
-
-// Con compilador
-new Vue({
-  el: '#app',
-  store,
-  router,
-  template: '<App/>',
-  components: { App }
-});
-
-/*
-// Sin compilador. Este es el recomendado
 new Vue({
   el: '#app',
   store,
   router,
   render: h => h(App)
 });
-*/
+
