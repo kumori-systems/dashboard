9e1f460254122e4bd6e8fa70d7b433150bd5f98b
Óscar Muñoz Garriós <omunoz@iti.es>
2017-07-25 13:36:31 +0200

    corregido pequeño error con el nombre de los runtime

--- src/store/classes/stamp_state/runtime.ts
+++ src/store/classes/stamp_state/runtime.ts
@@ -7,9 +7,10 @@ export default class Runtime implements StampElement {
     constructor(uri: string) {
         this.uri = uri;
         let splitted: Array<string> = this.uri.split('/');
+        console.log('Splitted vale', splitted);
         this.owner = splitted[2];
-        this.name = splitted[3];
-        for (let i = 4; i < splitted.length - 1; i++) {
+        this.name = splitted[4];
+        for (let i = 5; i < splitted.length - 1; i++) {
             this.name += '.' + splitted[i];
         }
         this.version = splitted[splitted.length - 1];
