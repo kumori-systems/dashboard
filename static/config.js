cc1153a35c08b6479fc2f2df11a368c7daf51109
Óscar Muñoz <osmuogar@inf.upv.es>
2017-09-26 09:41:57 +0200

    stamp url updated

--- static/config.js
+++ static/config.js
@@ -1,2 +1,2 @@
-export const ACS_URI = "http://acs-dame-argo.slap53.iti.es/acs";
-export const ADMISSION_URI = "http://admission-dame-argo.slap53.iti.es/admission"
+export const ACS_URI = "http://acs.argo.kumori.cloud/acs";
+export const ADMISSION_URI = "http://admission.argo.kumori.cloud/admission"
