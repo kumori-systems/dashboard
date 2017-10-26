fb334fd5ab3c99584570ac174706d83e5db36442
Óscar Muñoz <osmuogar@inf.upv.es>
2017-10-26 10:09:36 +0200

    Disabed button when no file is chosen
    On NewBundle view the upload bundle button is dissabled when no file is chosen

--- src/components/views/innerComponents/form/NewBundle.vue
+++ src/components/views/innerComponents/form/NewBundle.vue
@@ -1,33 +1,33 @@
 <template>
     <div>
-        <input type="file" class="box" v-on:change="onFileChange">
-        <div>
-            <button class="button is-primary" v-on:click="addElement">Upload bundle</button>
-        </div>
+      <input type="file" class="box" v-on:change="onFileChange">  
+      <button v-bind:disabled="fileList === null || fileList.length === 0" class="button is-primary" v-on:click="addElement">Upload bundle</button>
+    
     </div>
 </template>
 
 <script lang="ts">
-
-import Vue from 'vue';
-import Component from 'vue-class-component';
+import Vue from "vue";
+import Component from "vue-class-component";
 
 @Component({
-    name: 'NewBundle'
+  name: "NewBundle"
 })
 export default class NewBundle extends Vue {
-    fileList: FileList;
+  fileList: FileList = null;
 
-    onFileChange(e) {
-        this.fileList = (<HTMLInputElement>e.target).files || (<DataTransfer>e.dataTransfer).files;
+  onFileChange(e) {
+    console.log('Llamamos a onFileChange');
+    this.fileList =
+      (<HTMLInputElement>e.target).files ||
+      (<DataTransfer>e.dataTransfer).files;
+  }
+
+  addElement() {
+    if (this.fileList!==null) {
+      this.$store.dispatch("addNewBundle", this.fileList.item(0));
+      this.$router.push("/elements");
     }
-
-    addElement() {
-        if (this.fileList) {
-            this.$store.dispatch('addNewBundle', this.fileList.item(0));
-            this.$router.push('/elements');
-        }
-    }
-
+  }
 }
 </script>
