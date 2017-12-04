1006c0a78dcfbdd86982559b55c861410d19f93b
Óscar Muñoz <osmuogar@inf.upv.es>
2017-12-04 13:16:33 +0100

    adding commented local-stamp config

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,6 @@
 export const ACS_URI = "http://acs.argo.kumori.cloud/acs";
 export const ADMISSION_URI = "https://admission.argo.kumori.cloud/admission";
+
+// localstamp config
+// export const ACS_URI = "http://localhost:8090/acs";
+// export const ADMISSION_URI = "http://localhost:8090/admission";
