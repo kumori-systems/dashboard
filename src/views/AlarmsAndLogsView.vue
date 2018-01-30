37329f057a34c4cfde8dd47307143755c0f27131
Óscar Muñoz <osmuogar@inf.upv.es>
2018-01-30 10:26:46 +0100

    completely functional date filter

--- src/views/AlarmsAndLogsView.vue
+++ src/views/AlarmsAndLogsView.vue
@@ -14,20 +14,84 @@
       <v-layout>
 
         <!-- Level filter -->
-        <v-flex xs12 md2 lg2>
+        <v-flex xs12 md2>
           <v-select label="Minimum log level" v-model="selectedLogLevel" v-bind:items="logLevels"></v-select>
         </v-flex>
 
-        <v-spacer></v-spacer>
+        <!-- Date filter -->
+        <v-flex xs12 md6>
+          <v-layout>
+
+            <!-- From date picker -->
+            <v-flex xs12 md3 class="pl-3">
+              <v-menu lazy v-on:close-on-content-click="false" v-model="showFromDateMenu"
+                transition="scale-transition" offset-y full-width
+                v-on:nudge-right="40" max-width="290px" min-width="290px">
+                <v-text-field slot="activator" label="From date"
+                  v-model="fromDateFormatted" prepend-icon="event" 
+                  @blur="date = parseDate(fromDateFormatted)">
+                </v-text-field>
+                <v-date-picker v-model="fromDate" no-title scrollable actions
+                  @input="fromDateFormatted = formatDate($event)">
+                </v-date-picker>
+              </v-menu>
+            </v-flex>
+            
+            <!-- From time picker -->
+            <v-flex xs12 md3 class="pr-1">
+              <v-menu lazy v-model="showFromTimeMenu"
+                transition="scale-transition" offset-y full-width
+                v-on:nudge-right="40" max-width="290px" min-width="290px">
+                <v-text-field slot="activator" label="From time"
+                  v-model="fromTime" prepend-icon="query_builder" >
+                </v-text-field>
+                <v-time-picker v-model="fromTime" format="24hr">
+                </v-time-picker>
+              </v-menu>
+            </v-flex>
+
+            <!-- To date picker -->
+            <v-flex xs12 md3  class="pl-1">
+              <v-menu lazy v-on:close-on-content-click="false" v-model="showToDateMenu"
+                transition="scale-transition" offset-y full-width
+                v-on:nudge-right="40" max-width="290px" min-width="290px">
+                <v-text-field slot="activator" label="To date"
+                  v-model="toDateFormatted" prepend-icon="event" 
+                  @blur="date = parseDate(toDateFormatted)">
+                </v-text-field>
+                <v-date-picker v-model="toDate" no-title scrollable actions
+                  @input="toDateFormatted = formatDate($event)">
+                </v-date-picker>
+              </v-menu>
+            </v-flex>
+            
+            <!-- To time picker -->
+            <v-flex xs12 md3  class="pr-3">
+              <v-menu lazy v-model="showToTimeMenu"
+                transition="scale-transition" offset-y full-width
+                v-on:nudge-right="40" max-width="290px" min-width="290px">
+                <v-text-field slot="activator" label="To time"
+                  v-model="toTime" prepend-icon="query_builder" >
+                </v-text-field>
+                <v-time-picker v-model="toTime" format="24hr">
+                </v-time-picker>
+              </v-menu>
+            </v-flex>
+
+          </v-layout>
+        </v-flex>
         
+        <v-spacer></v-spacer>
+
         <!-- Search filter -->
-        <v-flex xs12 md4 lg4>
+        <v-flex xs12 md4>
           <v-text-field label="Search" v-model="search" prepend-icon="search">
           </v-text-field>
         </v-flex>
 
       </v-layout>
 
+      <!-- Log table -->
       <v-data-table v-bind:headers="headers" v-bind:items="logs" hide-actions  v-bind:search="search">
 
         <template slot="items" scope="props">
@@ -53,6 +117,7 @@
 
     </v-container>
 
+    <!-- Detailed info dialog -->
     <v-dialog v-model="logInfoDialog" max-width="800px">
       <v-card>
         <v-card-title class="headline">Log info</v-card-title>
@@ -80,13 +145,7 @@ const NUM_ITEMS_PER_PAGE = 20;
   name: "alarms-and-logs-view"
 })
 export default class AlarmsAndLogsView extends Vue {
-  logLevels: string[] = ["Debug", "Info", "Warning", "Error"];
-  selectedLogLevel: string = "Debug";
-  numPages: number = 1;
-  actualPage: number = 1;
-  logInfoDialog: boolean = false;
-  data: string = "";
-  search: string = "";
+  /** Table headers. */
   headers: any[] = [
     {
       text: "",
@@ -114,6 +173,47 @@ export default class AlarmsAndLogsView extends Vue {
     }
   ];
 
+  /** Detailed log dialog. */
+  logInfoDialog: boolean = false;
+
+  /** Detailed log data. */
+  data: string = "";
+
+  /** Search pattern. */
+  search: string = "";
+
+  /** Selectable log levels. */
+  logLevels: string[] = ["Debug", "Info", "Warning", "Error"];
+  /** Selected minimum log level. */
+  selectedLogLevel: string = "Debug";
+
+  /** Total number of pages. */
+  numPages: number = 1;
+  /** Actual page. */
+  actualPage: number = 1;
+
+  /** Show/Hide from date selector. */
+  showFromDateMenu: boolean = false;
+  /** Show/Hide from time selector. */
+  showFromTimeMenu: boolean = false;
+  /** Selected from date */
+  fromDate: string = null;
+  /** Selected from date formatted. */
+  fromDateFormatted: string = null;
+  /** Selected from time. */
+  fromTime: string = null;
+
+  /** Show/Hide to date selector. */
+  showToDateMenu: boolean = false;
+  /** Show/Hide to time selector. */
+  showToTimeMenu: boolean = false;
+  /** Selected to date */
+  toDate: string = null;
+  /** Selected to date formatted. */
+  toDateFormatted: string = null;
+  /** Selected to time. */
+  toTime: string = null;
+
   get logs() {
     let logs: Notification[] = this.$store.getters.notifications;
 
@@ -121,37 +221,110 @@ export default class AlarmsAndLogsView extends Vue {
     this.numPages = logs.length / NUM_ITEMS_PER_PAGE;
 
     return logs
-      .slice(
-        (this.actualPage-1) * NUM_ITEMS_PER_PAGE,
-        (this.actualPage-1) * NUM_ITEMS_PER_PAGE + NUM_ITEMS_PER_PAGE
-      )
-      .filter((n, index, arrayfun) => {
+      .filter((item, index, arrayfun) => {
+        // fromDate
+        if (this.fromDate && this.fromDate !== "") {
+          let date = new Date(this.fromDate);
+          return (
+            item.time.getFullYear() >= date.getFullYear() &&
+            item.time.getMonth() >= date.getMonth() &&
+            item.time.getDate() >= date.getDate()
+          );
+        }
+        return true;
+      })
+      .filter((item, index, arrayfun) => {
+        // fromTime
+        if (this.fromTime && this.fromTime !== "") {
+          let date = this.fromTime.split(":");
+          return (
+            item.time.getHours() >= parseInt(date[0]) &&
+            item.time.getMinutes() >= parseInt(date[1])
+          );
+        }
+        return true;
+      })
+      .filter((item, index, arrayfun) => {
+        // toDate
+        if (this.toDate && this.toDate !== "") {
+          let date = new Date(this.toDate);
+          return (
+            item.time.getFullYear() <= date.getFullYear() &&
+            item.time.getMonth() <= date.getMonth() &&
+            item.time.getDate() <= date.getDate()
+          );
+        }
+        return true;
+      })
+      .filter((item, index, arrayfun) => {
+        // toTime
+        if (this.toTime && this.toTime !== "") {
+            let date = this.toTime.split(":");
+          return (
+            item.time.getHours() <= parseInt(date[0]) &&
+            item.time.getMinutes() <= parseInt(date[1])
+          );
+        }
+        return true;
+      })
+      .filter((item, index, arrayfun) => {
         switch (this.selectedLogLevel) {
           case "Info":
             return (
-              n.level === Notification.LEVEL.INFO ||
-              n.level === Notification.LEVEL.WARNING ||
-              n.level === Notification.LEVEL.ERROR
+              item.level === Notification.LEVEL.INFO ||
+              item.level === Notification.LEVEL.WARNING ||
+              item.level === Notification.LEVEL.ERROR
             );
 
           case "Warning":
             return (
-              n.level === Notification.LEVEL.WARNING ||
-              n.level === Notification.LEVEL.ERROR
+              item.level === Notification.LEVEL.WARNING ||
+              item.level === Notification.LEVEL.ERROR
             );
           case "Error":
-            return n.level === Notification.LEVEL.ERROR;
+            return item.level === Notification.LEVEL.ERROR;
 
           default:
             return true;
         }
-      }, this);
+      }, this)
+      .slice(
+        (this.actualPage - 1) * NUM_ITEMS_PER_PAGE,
+        (this.actualPage - 1) * NUM_ITEMS_PER_PAGE + NUM_ITEMS_PER_PAGE
+      );
   }
+
+  formatDate(date) {
+    if (!date) {
+      return null;
+    }
+
+    const [year, month, day] = date.split("-");
+    return `${day}-${month}-${year}`;
+  }
+
+  parseDate(date) {
+    if (!date) {
+      return null;
+    }
+
+    const [day, month, year] = date.split("-");
+    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+  }
+
+  /*
+  get formatedSelectedFromDate() {
+    return (this.selectedFromDate || "") + " " + (this.fromTime || "");
+  }
+
+  get formatedSelectedToDate() {
+    return (this.selectedToDate || "") + " " + (this.toTime || "");
+  }
+*/
 
   showLogInfoDialog(data: string) {
     this.data = data;
     this.logInfoDialog = true;
   }
-
 }
 </script>
