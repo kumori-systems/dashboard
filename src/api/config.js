6a3da49a91c1e910766012ec34d9a8ee829e6bdf
Óscar Muñoz <osmuogar@inf.upv.es>
2018-02-19 12:39:00 +0100

    default config

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 // production config
-// export const ACS_URI = "https://acs.argo.kumori.cloud/acs";
-// export const ADMISSION_URI = "https://admission.argo.kumori.cloud/admission";
+export const ACS_URI = "https://acs.argo.kumori.cloud/acs";
+export const ADMISSION_URI = "https://admission.argo.kumori.cloud/admission";
 
 // localstamp config
 // export const ACS_URI = "http://localhost:8090/acs";
@@ -10,9 +10,9 @@
 // export const ACS_URI = "http://acs-jvalero.test.kumori.cloud/acs";
 // export const ADMISSION_URI = "http://admission-jvalero.test.kumori.cloud/admission";
 
-// jferrer config
-export const ACS_URI = "http://acs-latest.test.kumori.cloud/acs";
-export const ADMISSION_URI = "http://admission-latest.test.kumori.cloud/admission";
+// latest config
+// export const ACS_URI = "http://acs-latest.test.kumori.cloud/acs";
+// export const ADMISSION_URI = "http://admission-latest.test.kumori.cloud/admission";
 
 // fail config
 // export const ACS_URI = "";
