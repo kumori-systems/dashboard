64480cd05d72d03dc62ea572a8917c432220b8a9
Óscar Muñoz <osmuogar@inf.upv.es>
2018-02-22 14:08:30 +0100

    setted erroneous config by default

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,7 @@
+// fail config
+export const ACS_URI = "";
+export const ADMISSION_URI = "";
+
 // production config
 // export const ACS_URI = "https://acs.argo.kumori.cloud/acs";
 // export const ADMISSION_URI = "https://admission.argo.kumori.cloud/admission";
@@ -11,10 +15,5 @@
 // export const ADMISSION_URI = "http://admission-jvalero.test.kumori.cloud/admission";
 
 // latest config
-export const ACS_URI = "http://acs-latest.test.kumori.cloud/acs";
-export const ADMISSION_URI = "http://admission-latest.test.kumori.cloud/admission";
-
-// fail config
-// export const ACS_URI = "";
-// export const ADMISSION_URI = "";
-
+// export const ACS_URI = "http://acs-latest.test.kumori.cloud/acs";
+// export const ADMISSION_URI = "http://admission-latest.test.kumori.cloud/admission";
