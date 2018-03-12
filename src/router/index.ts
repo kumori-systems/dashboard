7604af26aa60790c20a6b6793a59d7ff22caef5a
Óscar Muñoz <osmuogar@inf.upv.es>
2018-03-12 13:06:31 +0100

    when switching a page the scrollbar is on the top

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,7 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   base: '/',
+  scrollBehavior: (to, from, savedPosition) => { return { x: 0, y: 0 } },
   routes: [
     {
       path: '/overview',
